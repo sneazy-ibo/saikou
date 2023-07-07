203d0915d907a295f89c193444f44f4cc744c845
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2023-07-07 20:20:43 +0530

    Add new source AnimeDao, Fixes #524

--- app/src/main/java/ani/saikou/parsers/AnimeSources.kt
+++ app/src/main/java/ani/saikou/parsers/AnimeSources.kt
@@ -2,7 +2,17 @@ package ani.saikou.parsers
 
 import ani.saikou.Lazier
 import ani.saikou.lazyList
-import ani.saikou.parsers.anime.*
+import ani.saikou.parsers.anime.AllAnime
+import ani.saikou.parsers.anime.AnimeDao
+import ani.saikou.parsers.anime.AnimePahe
+import ani.saikou.parsers.anime.Gogo
+import ani.saikou.parsers.anime.Haho
+import ani.saikou.parsers.anime.HentaiFF
+import ani.saikou.parsers.anime.HentaiMama
+import ani.saikou.parsers.anime.HentaiStream
+import ani.saikou.parsers.anime.Marin
+import ani.saikou.parsers.anime.NineAnime
+import ani.saikou.parsers.anime.Zoro
 
 object AnimeSources : WatchSources() {
     override val list: List<Lazier<BaseParser>> = lazyList(
@@ -11,13 +21,13 @@ object AnimeSources : WatchSources() {
         "Zoro" to ::Zoro,
         "Marin" to ::Marin,
         "AnimePahe" to ::AnimePahe,
-//        "Enime" to ::Enime,
         "9anime" to ::NineAnime,
+        "AnimeDao" to ::AnimeDao
     )
 }
 
 object HAnimeSources : WatchSources() {
-    val aList: List<Lazier<BaseParser>>  = lazyList(
+    private val aList: List<Lazier<BaseParser>>  = lazyList(
         "HentaiMama" to ::HentaiMama,
         "Haho" to ::Haho,
         "HentaiStream" to ::HentaiStream,
