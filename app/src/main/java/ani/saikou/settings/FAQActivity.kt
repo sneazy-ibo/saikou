406c9df396ed89c08263cc3d81d61a6d6ff1ef48
SunglassJerry <96760535+SunglassJerry@users.noreply.github.com>
2023-07-15 22:20:01 +0600

    Updated Forks list and Added new FAQs + fixed the old ones. (#746)

--- app/src/main/java/ani/saikou/settings/FAQActivity.kt
+++ app/src/main/java/ani/saikou/settings/FAQActivity.kt
@@ -15,7 +15,9 @@ class FAQActivity : AppCompatActivity() {
 
         Triple(R.drawable.ic_round_help_24, currContext()!!.getString(R.string.question_1), currContext()!!.getString(R.string.answer_1)),
         Triple(R.drawable.ic_round_auto_awesome_24, currContext()!!.getString(R.string.question_2), currContext()!!.getString(R.string.answer_2)),
+        Triple(R.drawable.ic_round_auto_awesome_24, currContext()!!.getString(R.string.question_17), currContext()!!.getString(R.string.answer_17)),
         Triple(R.drawable.ic_round_download_24, currContext()!!.getString(R.string.question_3), currContext()!!.getString(R.string.answer_3)),
+        Triple(R.drawable.ic_round_help_24, currContext()!!.getString(R.string.question_16), currContext()!!.getString(R.string.answer_16)),
         Triple(R.drawable.ic_round_dns_24, currContext()!!.getString(R.string.question_4), currContext()!!.getString(R.string.answer_4)),
         Triple(R.drawable.ic_baseline_screen_lock_portrait_24, currContext()!!.getString(R.string.question_5), currContext()!!.getString(R.string.answer_5)),
         Triple(R.drawable.ic_anilist, currContext()!!.getString(R.string.question_6), currContext()!!.getString(R.string.answer_6)),
